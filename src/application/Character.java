e7d18065cec7268bf2710ff4a64e335980542c0a
eirik <eirsut@gmail.com>
2019-01-30 15:31:03 +0100

    Initial commit

---
+++ src/application/Character.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Character  {
+	
+	//The purpose of this class is data storage from the controller, as well as file reading-writing upon saving or loading a character.
+	//The controller class must fetch data from, and store it here as the input fields in the UI are edited by the user.
+	
+	private String name;
+	private String characterClass;
+	
+	
+
+}
---
+++ src/application/SampleController.java
@@ -0,0 +1,28 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class SampleController {
+	
+	//Controller class for the FXML file. 
+	
+	@FXML private TextField name;
+	@FXML private TextField characterClass;
+	@FXML private TextField level;
+	@FXML private TextField race;
+	@FXML private TextField subrace;
+	
+	public SampleController() {
+		
+	}
+	
+	@FXML
+	private void initialize() {
+		
+	}
+	
+	
+}
